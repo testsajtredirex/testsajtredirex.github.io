bc4fdb3bd64a9c19e39cb7027eabfe9d6d4c0294
testsajtredirex <164942265+testsajtredirex@users.noreply.github.com>
2024-05-25 16:42:24 +0200

    asd
    asd

--- JedanKlik/assets/js/custom.js
+++ JedanKlik/assets/js/custom.js
@@ -126,23 +126,3 @@
 })(window.jQuery);
 
 
-// Get the modal
-var modal = document.getElementById("imageModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.querySelector(".image-click");
-var modalImg = document.getElementById("imgModal");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
